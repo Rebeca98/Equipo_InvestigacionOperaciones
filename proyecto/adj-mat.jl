d42b6ac78fff2e1a41649b439f3e22e0f8ae8013
Alonso Martinez <alonso.mtz99@gmail.com>
2021-05-18 11:14:42 -0500

    Guardando matrices salvo las enormes

--- proyecto/adj-mat.jl
+++ proyecto/adj-mat.jl
@@ -6,39 +6,52 @@ using CSV # Para leer el .csv
 using LinearAlgebra # Para norm, entre otros
 using NPZ # Para escribir la matriz en formato .npy para que la lea numpy
 
-# Cantidad de lineas
-n = (open("csv/Oman.csv") |> readlines |> length) -1
+# Leyendo archivos por archivo en raw_ciudades
+for file in readdir("csv/")
 
-# Guardamos los puntos
-puntos = Vector{Vector{Float64}}(undef, n)
+	# filename es el nombre de archivo tomado a partir del root (/proyecto)
+	filename = "csv/" * file
 
-# Contador para las columnas
-k = 1
+	# Cantidad de lineas
+	n = (open(filename) |> readlines |> length) -1
 
-# Iterando sobre columna
-for row in CSV.Rows("csv/Oman.csv")
-	# Obtenemos los datos y los parseamos como Float64
-	x = parse(Float64, row.x)
-	y = parse(Float64, row.y)
+	println("Procesando $(n) líneas de $(filename)")
 
-	# Suena poco idiomático pero para esto me da por ahora
-	puntos[k] = [x, y]
+	# Guardamos los puntos
+	puntos = Vector{Vector{Float64}}(undef, n)
 
-	# Julia pide global porque k se definió fuera de este scope
-	global k = k+1
-end
+	# Contador para las columnas
+	global k = 1
 
+	# Iterando sobre columna
+	for row in CSV.Rows(filename)
+		# Obtenemos los datos y los parseamos como Float64
+		x = parse(Float64, row.x)
+		y = parse(Float64, row.y)
 
-# Ahora si matriz de adyacencias
-ady = zeros(n,n)
+		# Suena poco idiomático pero para esto me da por ahora
+		puntos[k] = [x, y]
 
-# Llenamos la matriz de adyacencias por columna porque Julia usa column-major storage
-for i = 1:n
-	for j = i:n
-		# Calculando la distancia entre puntos como la norma de su resta
-		ady[i, j] = norm(puntos[i] - puntos[j])
+		# Julia pide global porque k se definió fuera de este scope
+		global k = k+1
 	end
-end
 
-# Guardando en formato de matriz matplotlib
-npzwrite("mats/oman.npy", ady)
+
+	# Ahora si matriz de adyacencias
+	ady = zeros(n,n)
+
+	# Llenamos la matriz de adyacencias por columna porque Julia usa column-major storage
+	for i = 1:n
+		for j = i:n
+			# Calculando la distancia entre puntos como la norma de su resta
+			ady[i, j] = norm(puntos[i] - puntos[j])
+		end
+	end
+
+	# Guardando en formato de matriz numpy
+	pais = split(file, ".")[1]
+
+	println("Guardando matriz de adyacencia en $(pais).npy")
+
+	npzwrite("mats/$(pais).npy", ady)
+end
